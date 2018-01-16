ee48b8bc693aebe7d1b130e042227f102c458752
Javito <javier.martinezrodriguez86@gmail.com>
2018-01-16 19:54:54 +0100

    16/01/18

--- GestionEventos/ViewModel/InicioViewModel.cs
+++ GestionEventos/ViewModel/InicioViewModel.cs
@@ -6,25 +6,51 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using GestionEventos.Model;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using MvvmDialogs.ViewModels;
+using GalaSoft.MvvmLight.Command;
+using GestionEventos.ViewModel;
 
 namespace GestionEventos.ViewModel
 {
     class InicioViewModel : INotifyPropertyChanged
     {
         gestioneventosEntities ctx = new gestioneventosEntities();
-        private List<evento> _eventos;
-        private List<local> _locales;
-        private List<cliente> _clientes;
-        private List<direccion> _direcciones;
-        private List<estilo> _estilos;
-        private List<flyer> _flyer;
-        private List<listaevento> _listaeventos;
-        private List<listafavorito> _listafavoritos;
-        private List<tipoevento> _tipoeventos;
-        private List<tipolocal> _tipolocales;        
-        private evento _selectedEvento;
-        private local _selectedLocal;
+        //private List<evento> _eventos;
+        //private List<local> _locales;
+        //private List<cliente> _clientes;
+        //private List<direccion> _direcciones;
+        //private List<estilo> _estilos;
+        //private List<flyer> _flyer;
+        //private List<listaevento> _listaeventos;
+        //private List<listafavorito> _listafavoritos;
+        //private List<tipoevento> _tipoeventos;
+        //private List<tipolocal> _tipolocales;        
+        //private evento _selectedEvento;
+        //private local _selectedLocal;
 
+        private ObservableCollection<IDialogViewModel> _Dialogs = new ObservableCollection<IDialogViewModel>();
+        public System.Collections.ObjectModel.ObservableCollection<IDialogViewModel> Dialogs { get { return _Dialogs; } }
+        
+        public ICommand LogInCommand
+        {
+            get { return new RelayCommand(LogIn); }
+        }
+        public void LogIn()
+        {
+            this.Dialogs.Add(new homeLocalViewModel
+            {
+                Title = "Afegir Contacte",                
+                OkText = "Ok",
+                TextEnabled = true,
+                OnOk = (sender) => { sender.Close(); },
+
+                OnCancel = (sender) => { sender.Close(); },
+                OnCloseRequest = (sender) => { sender.Close(); }
+            });
+        }
+        
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
---
+++ GestionEventos/ViewModel/homeLocalViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GestionEventos.Model;
+using System.Windows;
+using MvvmDialogs.ViewModels;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace GestionEventos.ViewModel
+{
+    class homeLocalViewModel : ViewModelBase, IUserDialogViewModel
+    {
+        private string _okText;
+        private bool _textEnabled;
+        private string _title;
+
+        public string OkText
+        {
+            get { return _okText; }
+            set { _okText = value; }
+        }
+        public bool TextEnabled
+        {
+            get { return _textEnabled; }
+            set { _textEnabled = value; }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }     
+        public virtual bool IsModal { get { return true; } }
+        public virtual void RequestClose() { this.DialogClosing(this, null); }
+        public virtual event EventHandler DialogClosing;      
+        //public event PropertyChangedEventHandler PropertyChanged;           
+
+        //private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        //{
+        //    if (PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+        public ICommand OkCommand { get { return new RelayCommand(Ok); } }
+        protected virtual void Ok()
+        {
+            if (this.OnOk != null)
+                this.OnOk(this);
+            else
+                Close();
+        }
+
+        public ICommand CancelCommand { get { return new RelayCommand(Cancel); } }
+        protected virtual void Cancel()
+        {
+            if (this.OnCancel != null)
+                this.OnCancel(this);
+            else
+                Close();
+        }
+
+        public void Close()
+        {
+            if (this.DialogClosing != null)
+                this.DialogClosing(this, new EventArgs());
+        }
+
+        public Action<homeLocalViewModel> OnOk { get; set; }
+        public Action<homeLocalViewModel> OnCancel { get; set; }       
+        public Action<homeLocalViewModel> OnCloseRequest { get; set; }
+    }
+}
