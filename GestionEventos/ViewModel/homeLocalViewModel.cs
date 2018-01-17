0c7912573bc536417975e33b6f0a04349aef3b27
ralor <alor.rocio@gmail.com>
2018-01-17 15:45:04 +0100

    jhg

--- GestionEventos/ViewModel/homeLocalViewModel.cs
+++ GestionEventos/ViewModel/homeLocalViewModel.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using GestionEventos.Model;
-using System.Windows;
 using MvvmDialogs.ViewModels;
-using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
+
 
 namespace GestionEventos.ViewModel
 {
