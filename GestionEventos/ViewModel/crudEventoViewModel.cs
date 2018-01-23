a976f9da5297543ca75e7c4c7ba009dc92102cdd
Javito <javier.martinezrodriguez86@gmail.com>
2018-01-23 18:41:13 +0100

    ñlç

--- GestionEventos/ViewModel/crudEventoViewModel.cs
+++ GestionEventos/ViewModel/crudEventoViewModel.cs
@@ -43,7 +43,8 @@ namespace GestionEventos.ViewModel
         }
         public void ListaEvento()
         {
-            TipoEventoss = ctx.TipoEventoes.Select(x => x).ToList();
+            TipoEventoss = ctx.TipoEventos.Select(x => x).ToList();
+            
         }
         public virtual bool IsModal { get { return true; } }
         public virtual void RequestClose() { this.DialogClosing(this, null); }
